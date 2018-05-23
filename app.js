60be67241e4adfeebcdbe94258c8e270a07c12f1
Boris Josz <Boris.Josz@gmail.com>
2018-05-23 10:49:28 +0200

    nodemon up and running, minimal setup server (restart point..)

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+const nodemailer = require('nodemailer');
+
+
+// BodyParser Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => res.send('BS server'))
+ 
+app.listen(3000, () => console.log('Server listening on port 3000!'))
+
+app.post('/', function (req, res) {
+    console.log(req.body)
+})
